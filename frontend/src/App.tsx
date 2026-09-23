14f80422f1f8a3b6adf2c52542ade960fc7b7a3a
juanRCoder <guillecarck@gmail.com>
2026-09-23 16:20:34 -0500

    init app front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button"
 function App() {
 
   return (
-    <div className="bg-[#16171d] m-auto min-h-screen outline flex items-center justify-center">
-      <Button className="text-center cursor-pointer">Hola mundo</Button>
+    <div className="m-auto min-h-screen outline flex items-center justify-center">
+      <Button variant='outline' className="text-center cursor-pointer">Hola mundo</Button>
     </div>
   )
 }
