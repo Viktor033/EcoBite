8399645543244d3c9560489083672612bf3fea79
juanRCoder <guillecarck@gmail.com>
2026-09-23 15:58:40 -0500

    fix: setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div className="bg-[#16171d] m-auto min-h-screen outline flex items-center justify-center">
+      <Button className="text-center cursor-pointer">Hola mundo</Button>
+    </div>
+  )
+}
+
+export default App
